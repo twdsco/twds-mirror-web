7886212e731cbee4d8bb37cf665b4ce308d77d30
Jasper Yu <007seadog@gmail.com>
2024-04-03 03:36:49 +0800

    Added parallel gen graph and hi dpi

--- gen_graph.sh
+++ gen_graph.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-W=545
-H=273
+W=800
+H=400
 tmp=$(mktemp -d)
+
 for t in "light" "dark"
 do
 for p in "1h" "3h" "6h" "24h"
 do
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?theme=$t&panelId=1&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_traffic_"$p".png
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?theme=$t&panelId=5&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_connection_"$p".png
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?theme=$t&panelId=3&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disk1_"$p".png
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?theme=$t&panelId=4&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disk2_"$p".png
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?theme=$t&panelId=6&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_cpu_"$p".png
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?theme=$t&panelId=7&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_memory_"$p".png
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=1&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_traffic_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=5&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_connection_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=3&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disk1_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=4&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disk2_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=6&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_cpu_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=7&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_memory_"$p".png &
+for pid in `jobs -p`
+do
+wait $pid
+done
+sleep 1
 done
 done
+
 mv "$tmp"/* /srv/tunasync/mirror-web/_site/static/status/
 rm -r "$tmp"
