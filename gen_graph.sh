be641a550a4a5372f04833057010de55065300f5
Jasper Yu <007seadog@gmail.com>
2024-04-04 04:12:33 +0800

    Added disk related graphs

--- gen_graph.sh
+++ gen_graph.sh
@@ -14,6 +14,8 @@ wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale
 wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=4&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disk2_"$p".png &
 wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=6&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_cpu_"$p".png &
 wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=7&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_memory_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=8&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disku_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=9&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_diskrw_"$p".png &
 for pid in `jobs -p`
 do
 wait $pid
