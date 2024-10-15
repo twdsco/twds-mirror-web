05d5c284e1631b6be09423d8f993d24b005dd1bf
腹黒い茶 <haraguroicha@tipsy.coffee>
2024-10-16 01:57:39 +0800

    prevent `the input device is not a TTY`

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
+[[ $- != *i* ]] && export it="-it"
 
-docker run -it --rm -v $DIR:/data tunathu/mirror-web
-docker run -it --rm -v $DIR:/data tunathu/mirror-web rm -v /data/_site/static/status/isoinfo.json /data/_site/static/tunasync.json
+docker run "${it}" --rm -v $DIR:/data tunathu/mirror-web
+docker run "${it}" --rm -v $DIR:/data tunathu/mirror-web rm -v /data/_site/static/status/isoinfo.json /data/_site/static/tunasync.json
 
 "$DIR/rebuild-iso.sh"
--- rebuild-iso.sh
+++ rebuild-iso.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
+[[ $- != *i* ]] && export it="-it"
 
-docker run -it --rm -v $DIR:/data -v /mirror:/mirror tunathu/mirror-web bash -c '/data/geninfo/genisolist.py | tee /data/_site/static/status/isoinfo.json'
+docker run "${it}" --rm -v $DIR:/data -v /mirror:/mirror tunathu/mirror-web bash -c '/data/geninfo/genisolist.py | tee /data/_site/static/status/isoinfo.json'
