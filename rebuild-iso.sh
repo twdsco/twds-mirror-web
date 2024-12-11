dcdc382f53a9d87b250a7f6496250d48b37b693c
james58899 <james59988@gmail.com>
2024-12-11 16:22:51 +0000

    Remove -it in rebuild iso script

--- rebuild-iso.sh
+++ rebuild-iso.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
-[[ $- != *i* ]] && export it="-it"
 
-docker run "${it}" --rm -v $DIR:/data -v /mirror:/mirror tunathu/mirror-web bash -c '/data/geninfo/genisolist.py | tee /data/_site/static/status/isoinfo.json'
+docker run --rm -v $DIR:/data -v /mirror:/mirror tunathu/mirror-web bash -c '/data/geninfo/genisolist.py | tee /data/_site/static/status/isoinfo.json'
