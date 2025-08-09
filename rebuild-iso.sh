6f6a4c221801a31d4e6aacba9e7ae4917f98a9ef
腹黒い茶 <haraguroicha@tipsy.coffee>
2025-08-10 00:03:26 +0800

    fix shortage of iso images in download link

--- rebuild-iso.sh
+++ rebuild-iso.sh
@@ -2,4 +2,7 @@
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 
-docker run --rm -v $DIR:/data -v /mirror:/mirror tunathu/mirror-web bash -c '/data/geninfo/genisolist.py | tee /data/_site/static/status/isoinfo.json'
+docker run --rm -v $DIR:/data \
+        -v /mirror:/mirror \
+        -v /mirror2:/mirror2 \
+        tunathu/mirror-web bash -c '/data/geninfo/genisolist.py | tee /data/_site/static/status/isoinfo.json'
