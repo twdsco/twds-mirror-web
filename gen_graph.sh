d2829cf440a5cabb1a9882b32947395d0f1cb578
james58899 <james59988@gmail.com>
2026-03-02 19:41:55 +0800

    Update grafana image gen

--- gen_graph.sh
+++ gen_graph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-W=800
-H=400
+W=640
+H=360
 tmp=$(mktemp -d)
 
 for t in "light" "dark"
@@ -9,14 +9,14 @@ do
 #for p in "1h" "3h" "6h" "24h"
 for p in "24h"
 do
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=1&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_traffic_"$p".png &
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=5&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_connection_"$p".png &
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=3&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disk1_"$p".png &
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=4&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disk2_"$p".png &
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=6&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_cpu_"$p".png &
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=7&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_memory_"$p".png &
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=8&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_disku_"$p".png &
-wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&theme=$t&panelId=9&from=now-$p&width="$W"&height="$H -O "$tmp"/"$t"_diskrw_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=1&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_traffic_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=5&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_connection_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=3&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_disk1_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=4&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_disk2_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=6&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_cpu_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=7&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_memory_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=8&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_disku_"$p".png &
+wget -q "http://127.0.0.1:3000/render/d-solo/ddhj7whaercw0e/mirror-monitor?scale=3&hideLogo=true&theme=$t&panelId=9&from=now-$p&width=$W&height=$H" -O "$tmp"/"$t"_diskrw_"$p".png &
 for pid in `jobs -p`
 do
 wait $pid
